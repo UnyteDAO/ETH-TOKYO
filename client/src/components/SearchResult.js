4c07c702c14920fe3e3cb3f8a4bf71a949757fc6
KaiHosoya <kaihosoya711@gmail.com>
2023-04-14 22:24:39 +0900

    追加

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { List, Paper, ListItem, ListItemText } from "@mui/material";
-import { Link } from "react-router-dom";
+import { List, Paper, ListItem, ListItemText, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const SearchResult = ({ data }) => {
+  const navigate =useNavigate()
+
+  const handleClick = (item) => {
+    navigate(`/details/${item.id}`, { state: item})
+  }
+
   return (
     <List>
       {data.map((item) => (
-        <Link to={`/detail/${item.id}`} key={item.id} style={{ textDecoration: 'none' }}>
+        <Button onClick={() => {handleClick(item)}} key={item.id} style={{ textDecoration: 'none' }}>
           <Paper>
             <ListItem button>
               <ListItemText primary={item.title} />
             </ListItem>
           </Paper>
-        </Link>
+        </Button>
       ))}
     </List>
   );
