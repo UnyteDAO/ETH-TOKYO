3802acf1215d514258da28b82eb773da3f1b963d
local user <36323477+ystgs@users.noreply.github.com>
2023-04-15 08:08:22 +0900

    contract actions

--- client/src/actions/contractActions.ts
+++ client/src/actions/contractActions.ts
@@ -5,7 +5,7 @@ import ABI from "../../../blockchain/artifacts/contracts/Unyte.sol/Unyte.json";
 const CONTRACT_ADDRESS = "0x23BA45ED0FFE9617d133630c4bd655131c175093";
 const UNYTE_ABI: any = ABI;
 
-export const fetchTokenInfo = async () => {
+export const setIpfsHash = async () => {
   // detectEthereumProvider provider
   const provider: any = await detectEthereumProvider();
   if (provider) {
@@ -18,26 +18,29 @@ export const fetchTokenInfo = async () => {
     // setup instance to call contract with JSON RPC
     const contract = new web3.eth.Contract(UNYTE_ABI, CONTRACT_ADDRESS);
 
-    // コントラクトから情報を取得する例
-    const name = await contract.methods.name().call();
-    const symbol = await contract.methods.symbol().call();
-    const decimals = await contract.methods.decimals().call();
-
-    console.log("Token Decimals:", decimals);
+    const res = await contract.methods.setIpfsHash().send();
+    console.log("res: ", res);
   } else {
     console.error("Please install MetaMask!");
   }
 };
 
-const provider = async () => {
-  // const web3 = new Web3(Web3.givenProvider);
-  // connect with metamask wallet
-  // const accounts = await web3.eth.requestAccounts();
-  // const account = accounts[0];
+export const getIpfsHashList = async () => {
+  // detectEthereumProvider provider
+  const provider: any = await detectEthereumProvider();
+  if (provider) {
+    // new instance of web3
+    const web3 = new Web3(Web3.givenProvider);
+    // connect with metamask wallet
+    const accounts = await web3.eth.requestAccounts();
+    const account = accounts[0];
+    console.log("Account: ", account);
+    // setup instance to call contract with JSON RPC
+    const contract = new web3.eth.Contract(UNYTE_ABI, CONTRACT_ADDRESS);
 
-  // setup instance to call contract with JSON RPC
-  const contract = new web3.eth.Contract(
-    UNYTE_ABI.abi as any[],
-    CONTRACT_ADDRESS
-  );
+    const list = await contract.methods.getIpfsHashList().call();
+    console.log("list: ", list);
+  } else {
+    console.error("Please install MetaMask!");
+  }
 };
