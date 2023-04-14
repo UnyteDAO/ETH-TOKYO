b6d3b350893439e70448ef8ca4017ba6dd14aa97
local user <36323477+ystgs@users.noreply.github.com>
2023-04-15 08:01:42 +0900

    hoge

--- client/src/actions/contractActions.ts
+++ client/src/actions/contractActions.ts
@@ -1,5 +1,43 @@
-import * as LitJsSdk from "@lit-protocol/lit-node-client";
-import { ethers } from "ethers";
-import { useCallback, useEffect, useState } from "react";
-import { loginToLitNode } from "../components/litTest";
-import { create } from "ipfs-http-client";
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
+import ABI from "../../../blockchain/artifacts/contracts/Unyte.sol/Unyte.json";
+
+const CONTRACT_ADDRESS = "0x23BA45ED0FFE9617d133630c4bd655131c175093";
+const UNYTE_ABI: any = ABI;
+
+export const fetchTokenInfo = async () => {
+  // detectEthereumProvider provider
+  const provider: any = await detectEthereumProvider();
+  if (provider) {
+    // new instance of web3
+    const web3 = new Web3(Web3.givenProvider);
+    // connect with metamask wallet
+    const accounts = await web3.eth.requestAccounts();
+    const account = accounts[0];
+    console.log("Account: ", account);
+    // setup instance to call contract with JSON RPC
+    const contract = new web3.eth.Contract(UNYTE_ABI, CONTRACT_ADDRESS);
+
+    // コントラクトから情報を取得する例
+    const name = await contract.methods.name().call();
+    const symbol = await contract.methods.symbol().call();
+    const decimals = await contract.methods.decimals().call();
+
+    console.log("Token Decimals:", decimals);
+  } else {
+    console.error("Please install MetaMask!");
+  }
+};
+
+const provider = async () => {
+  // const web3 = new Web3(Web3.givenProvider);
+  // connect with metamask wallet
+  // const accounts = await web3.eth.requestAccounts();
+  // const account = accounts[0];
+
+  // setup instance to call contract with JSON RPC
+  const contract = new web3.eth.Contract(
+    UNYTE_ABI.abi as any[],
+    CONTRACT_ADDRESS
+  );
+};
