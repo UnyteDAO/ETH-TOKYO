8196880e9499d7bc08ab7badfaaee3a4a02e1ff7
Rei Sugiura <reibomaru@gmail.com>
2023-04-15 03:20:28 +0900

    getIpfsHashListを実装

--- blockchain/contracts/Unyte.sol
+++ blockchain/contracts/Unyte.sol
@@ -5,12 +5,25 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Unyte {
-    mapping(address => mapping(address => bytes32)) internal ipfsStorage;
-    mapping(address => mapping(address => bytes32)) internal keyStorage;
+    mapping(address => mapping(address => bytes32)) internal ipfsHashStorage;
+    address[] ipfsKeys;
     
-    function setIpfs(address target, bytes32 iphsHash, bytes32 key) public {
-        ipfsStorage[target][msg.sender] = iphsHash;
-        keyStorage[target][msg.sender] = key;
+    function setIpfsHash(address target, bytes32 ipfsHash) public {
+        ipfsHashStorage[target][msg.sender] = ipfsHash;
+        for (uint256 i = 0; i < ipfsKeys.length; i++) {
+            if (ipfsKeys[i] == target) {
+                return;
+            }
+        }
+        ipfsKeys.push(address);
+    }
+
+    function getIpfsHashList() public view returns(address[] memory, bytes32[] memory) {
+        bytes32[] memory ipfsHashList;
+        for(uint256 i = 0; i < ipfsKeys.length; i++){
+            ipfsHashList.push(ipfsHashStorage[ipfsKeys[i]]);
+        }
+        return (ipfsKeys, ipfsHashList);
     }
 
     function ping() public pure returns(string memory) {
