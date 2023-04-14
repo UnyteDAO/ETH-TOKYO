9ea1ce81bd30323cd60442650ff5b040bd8bfe75
local user <36323477+ystgs@users.noreply.github.com>
2023-04-14 23:10:12 +0900

    recruiter

--- client/src/pages/Recruiter.tsx
+++ client/src/pages/Recruiter.tsx
@@ -1,8 +1,85 @@
-import { Typography } from "@mui/material";
 import React from "react";
+import {
+  TextField,
+  InputAdornment,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Avatar,
+  Button,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { ListWrapper, ListContent } from "../styled/styledComps";
+import FolderIcon from "@mui/icons-material/Folder";
+
+interface Data {
+  id: number;
+  name: string;
+}
+
+const data: Data[] = Array(10)
+  .fill(0)
+  .map((_, index) => ({
+    id: index,
+    name: `Item ${index + 1}`,
+  }));
 
 const Recruiter = () => {
-  return <p>hoge</p>;
+  const handleSearch = () => {
+    console.log("Search clicked");
+  };
+  const handleButtonClick = (itemId: number) => {
+    console.log(`Button clicked for item ${itemId}`);
+  };
+
+  return (
+    <ListWrapper>
+      <ListContent>
+        <Box>
+          <TextField
+            fullWidth
+            label="Search"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleSearch} edge="end">
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Box>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item.id}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={item.name} />
+                <ListItemSecondaryAction>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => handleButtonClick(item.id)}>
+                    Click me
+                  </Button>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </ListContent>
+    </ListWrapper>
+  );
 };
 
 export default Recruiter;
