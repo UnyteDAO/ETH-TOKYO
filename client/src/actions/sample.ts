784e4472f83e8c321dd3e5cdcd71f734dc3c9fd4
local user <36323477+ystgs@users.noreply.github.com>
2023-04-15 00:32:40 +0900

    delete sample file

--- client/src/actions/sample.ts
+++
@@ -1,7 +0,0 @@
-export default class ConvertUtils {
-  static unixtimestamp2Yyyymm(unixtimestamp: string): string {
-    const date = new Date(unixtimestamp);
-    const yyyyMm = date.getFullYear() + "/" + (date.getMonth() + 1);
-    return yyyyMm;
-  }
-}
