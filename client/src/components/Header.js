f1022199f3e933283ca7b7281d21d2e4f16f7c76
KaiHosoya <kaihosoya711@gmail.com>
2023-04-14 23:27:10 +0900

    reviewerページとheader

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, Container, Grid, Card, CardContent, CardMedia, CardActionArea, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { styled, createTheme } from '@mui/system';
 
 const theme = createTheme();
@@ -11,20 +11,30 @@ const StyledAppBar = styled(AppBar)(({ theme }) => ({
 
 const Header = () => {
   const navigate = useNavigate();
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  if (window.ethereum) {
+    window.ethereum.request({ method: 'eth_requestAccounts' }).then((res) => {
+      setWalletAddress(res[0]);
+    });
+  }
 
   return (
     <StyledAppBar position="static">
-    <Toolbar>
-      <Box sx={{ flexGrow: 1 }}>
-        <Button onClick={() => navigate("/")} color="inherit" sx={{ textTransform: 'none' }}>
-          <Typography variant="h6">
-            Unyte
-          </Typography>
-        </Button>
-      </Box>
-    </Toolbar>
-  </StyledAppBar>
-  )
-}
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
+          <Button onClick={() => navigate("/")} color="inherit" sx={{ textTransform: 'none' }}>
+            <Typography variant="h6">Unyte</Typography>
+          </Button>
+        </Box>
+        {!walletAddress ? (
+          <Button color="secondary">Connect to Metamask</Button>
+        ) : (
+          <Typography>{walletAddress}</Typography>
+        )}
+      </Toolbar>
+    </StyledAppBar>
+  );
+};
 
 export default Header;
--- client/src/pages/Reviewer.tsx
+++ client/src/pages/Reviewer.tsx
@@ -1,22 +1,50 @@
-import React from "react";
-import { Typography, TextField, Button } from "@mui/material";
+import React from 'react';
+import { Typography, TextField, Button, Box, Grid } from '@mui/material';
 
 const Reviewer = () => {
   return (
     <main>
-      <div>
-        <Typography>To</Typography>
-        <TextField />
-      </div>
-      <div>
-        <Typography>Good</Typography>
-        <TextField />
-      </div>
-      <div>
-        <Typography>More</Typography>
-        <TextField />
-      </div>
-      <Button>Submit</Button>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '100vh',
+        }}
+      >
+        <Grid container justifyContent="center" alignItems="flex-start" spacing={2}>
+          <Grid item xs={8}>
+            <Box sx={{ position: 'relative' }}>
+              <Typography variant="h5" sx={{ position: 'absolute', top: '-30px', left: '0px' }}>
+                To
+              </Typography>
+              <TextField fullWidth />
+            </Box>
+          </Grid>
+        </Grid>
+        <Box sx={{ mt: 3 }}>
+          <Grid container justifyContent="center" alignItems="center" spacing={2}>
+            <Grid item>
+              <Typography variant="h5">Good</Typography>
+            </Grid>
+            <Grid item>
+              <TextField />
+            </Grid>
+          </Grid>
+        </Box>
+        <Grid container justifyContent="center" alignItems="center" spacing={2}>
+          <Grid item>
+            <Typography variant="h5">More</Typography>
+          </Grid>
+          <Grid item>
+            <TextField />
+          </Grid>
+        </Grid>
+        <Box sx={{ mt: 3 }}>
+          <Button variant="contained" size="large">Submit</Button>
+        </Box>
+      </Box>
     </main>
   );
 };
